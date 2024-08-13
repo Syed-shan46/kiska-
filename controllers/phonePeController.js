4f06a2e0d3581490db8afe9b00b1863999b1dcea
Syed-shan46 <159760172+Syed-shan46@users.noreply.github.com>
2024-08-13 16:00:54 +0530

    updated policy

---
+++ controllers/phonePeController.js
@@ -0,0 +1,4 @@
+const PHONE_PE_HOST_URL = 'https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay'
+const MERCHANT_ID = 'UATM221LS4ADJ5UN'
+const SALT_INDEX = 1
+const SALT_KEY = 'accaf302-67f3-4b26-8142-6066047d40fa'
