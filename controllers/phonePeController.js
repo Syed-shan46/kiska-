617892c8361e60e91146a1c405515e4adab11790
Syed-shan46 <159760172+Syed-shan46@users.noreply.github.com>
2024-08-13 20:56:00 +0530

    i adding phonepe integration

--- controllers/phonePeController.js
+++ controllers/phonePeController.js
@@ -1,108 +1,94 @@
-// const axios = require('axios');
-// const uniqid = require('uniqid');
-// const sha256 = require('sha256');
-// const PHONE_PE_HOST_URL = 'https://api-preprod.phonepe.com/apis/pg-sandbox'
-// const MERCHANT_ID = 'UATM221LS4ADJ5UN'
-// const SALT_INDEX = 1
-// const SALT_KEY = 'accaf302-67f3-4b26-8142-6066047d40fa'
-// const payEndPoint = '/pg/v1/pay';
-// const merchantTransactionId = uniqid();
-// const userId = 123;
+const axios = require('axios');
+const uniqid = require('uniqid');
+const sha256 = require('sha256');
+const PHONE_PE_HOST_URL = 'https://api-preprod.phonepe.com/apis/pg-sandbox'
+const MERCHANT_ID = 'UATM221LS4ADJ5UN'
+const SALT_INDEX = 1
+const SALT_KEY = 'accaf302-67f3-4b26-8142-6066047d40fa'
+const payEndPoint = '/pg/v1/pay';
+const merchantTransactionId = uniqid();
+const userId = 123;
 
-// payController = (req, res) => {
+payController = (req, res) => {
 
 
-//     const payLoad = {
-//         "merchantId": MERCHANT_ID,
-//         "merchantTransactionId": merchantTransactionId,
-//         "merchantUserId": userId,
-//         "amount": 30000,
-//         "redirectUrl": `http://localhost:3000/redirect-url/${merchantTransactionId}`,
-//         "redirectMode": "REDIRECT",
-//         "mobileNumber": "9999999999",
-//         "paymentInstrument": {
-//             "type": "PAY_PAGE"
-//         }
-//     };
+    const payLoad = {
+        "merchantId": MERCHANT_ID,
+        "merchantTransactionId": merchantTransactionId,
+        "merchantUserId": userId,
+        "amount": 30000,
+        "redirectUrl": `http://localhost:3000/redirect-url/${merchantTransactionId}`,
+        "redirectMode": "REDIRECT",
+        "mobileNumber": "9999999999",
+        "paymentInstrument": {
+            "type": "PAY_PAGE"
+        }
+    };
 
-//     const bufferObj = Buffer.from(JSON.stringify(payLoad), 'utf8');
-//     const base63EncodedPayLoad = bufferObj.toString('base64');
-//     const xVerify = sha256(base63EncodedPayLoad + payEndPoint + SALT_KEY) + '###' + SALT_INDEX
+    const bufferObj = Buffer.from(JSON.stringify(payLoad), 'utf8');
+    const base63EncodedPayLoad = bufferObj.toString('base64');
+    const xVerify = sha256(base63EncodedPayLoad + payEndPoint + SALT_KEY) + '###' + SALT_INDEX
 
-//     const options = {
-//         method: 'post',
-//         url: `${PHONE_PE_HOST_URL}${payEndPoint}`,
-//         headers: {
-//             accept: 'application/json',
-//             'Content-Type': 'application/json',
-//             'X-VERIFY': xVerify,
-//         },
-//         data: {
-//             request: base63EncodedPayLoad,
-//         }
-//     };
-//     axios
-//         .request(options)
-//         .then(function (response) {
-//             console.log(response.data);
-//             const url = response.data.data.instrumentResponse.redirectInfo.url;
-//             res.redirect(url);
-//             //res.send({ url });
-//         })
-//         .catch(function (error) {
-//             console.error(error);
-//         });
-// }
+    const options = {
+        method: 'post',
+        url: `${PHONE_PE_HOST_URL}${payEndPoint}`,
+        headers: {
+            accept: 'application/json',
+            'Content-Type': 'application/json',
+            'X-VERIFY': xVerify,
+        },
+        data: {
+            request: base63EncodedPayLoad,
+        }
+    };
+    axios
+        .request(options)
+        .then(function (response) {
+            console.log(response.data);
+            const url = response.data.data.instrumentResponse.redirectInfo.url;
+            res.redirect(url);
+            //res.send({ url });
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+}
 
-// // statusController = (req, res) => {
-// //     const { merchantTransactionId } = req.params;
-// //     if (merchantTransactionId) {
-// //         const xVerify = sha256(`/pg/v1/status/${MERCHANT_ID}/${merchantTransactionId}` + SALT_KEY) + '###' + SALT_INDEX
-// //         const options = {
-// //             method: 'get',
-// //             url: `${PHONE_PE_HOST_URL}/pg/v1/status/${MERCHANT_ID}/${merchantTransactionId}`,
-// //             headers: {
-// //                 accept: 'application/json',
-// //                 'Content-Type': 'application/json',
-// //                 "X-MERCHANT-ID": merchantTransactionId,
-// //                 'X-VERIFY': xVerify,
-// //             },
+statusController = (req, res) => {
+    const { merchantTransactionId } = req.params;
+    if (merchantTransactionId) {
+        const xVerify = sha256(`/pg/v1/status/${MERCHANT_ID}/${merchantTransactionId}` + SALT_KEY) + '###' + SALT_INDEX
+        const options = {
+            method: 'get',
+            url: `${PHONE_PE_HOST_URL}/pg/v1/status/${MERCHANT_ID}/${merchantTransactionId}`,
+            headers: {
+                accept: 'application/json',
+                'Content-Type': 'application/json',
+                "X-MERCHANT-ID": merchantTransactionId,
+                'X-VERIFY': xVerify,
+            },
 
-// //         };
-// //         axios
-// //             .request(options)
-// //             .then(function (response) {
-// //                 console.log(response.data);
-// //                 if (response.data.code === 'PAYMENT_SUCCESS') {
+        };
+        axios
+            .request(options)
+            .then(function (response) {
+                console.log(response.data);
+                if (response.data.code === 'PAYMENT_SUCCESS') {
                     
-// //                 }
-// //                 else if (response.data.code === 'PAYMENT_ERROR') {
-// //                     // {
-// //                     //     "success": false,
-// //                     //     "code": "PAYMENT_ERROR",
-// //                     //     "message": "Payment Failed",
-// //                     //     "data": {
-// //                     //       "merchantId": "PGTESTPAYUAT",
-// //                     //       "merchantTransactionId": "MT7850590068188104",
-// //                     //       "transactionId": "T2111221437456190170379",
-// //                     //       "amount": 100,
-// //                     //       "state": "FAILED",
-// //                     //       "responseCode": "ZM",
-// //                     //       "responseCodeDescription": "Invalid m-pin entered",
-// //                     //       "paymentInstrument": null
-// //                     //     }
-// //                     //   }
+                }
+                else if (response.data.code === 'PAYMENT_ERROR') {
+                    
 
-// //                 }
-// //             })
-// //             .catch(function (error) {
-// //                 console.error(error);
-// //             });
-// //         res.send({ merchantTransactionId });
-// //     }
-// //     else {
-// //         res.send({ error: 'Error' });
-// //     }
-// // }
+                }
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+        res.send({ merchantTransactionId });
+    }
+    else {
+        res.send({ error: 'Error' });
+    }
+}
 
-// module.exports = { payController }
+module.exports = { payController, statusController }
--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ const { viewSingleItem } = require('../controllers/productController');
 const { addAddress } = require('../controllers/addressController');
 const { checkoutPage } = require('../controllers/checkoutController');
 const { storePage, categoryList, categoryColumns, category2Columns } = require('../controllers/storeController');
-//const { payController, } = require('../controllers/phonePeController');
+const { payController, statusController } = require('../controllers/phonePeController');
 
 
 /* GET users listing. */
@@ -64,6 +64,6 @@ router.get('/privacy', (req, res) => {
     res.render('user/pv');
 })
 
-//router.get('/pay', payController);
+router.get('/pay', payController);
 
-//router.get('/redirect-url/:merchantTransactionId', statusController);
+router.get('/redirect-url/:merchantTransactionId', statusController);
