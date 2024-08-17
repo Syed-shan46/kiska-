a8e6f90c49c387f687e184974346342ddc3b34ac
Syed-shan46 <159760172+Syed-shan46@users.noreply.github.com>
2024-08-17 20:45:08 +0530

    rta

--- controllers/phonePeController.js
+++ controllers/phonePeController.js
@@ -1,11 +1,13 @@
 const axios = require('axios');
 const uniqid = require('uniqid');
+const nodemailer = require('nodemailer');
 const sha256 = require('sha256');
+require('dotenv').config();
+const Order = require('../models/order_model');
 const PHONE_PE_HOST_URL = 'https://api.phonepe.com/apis/hermes'
 const SALT_INDEX = 1
 const payEndPoint = '/pg/v1/pay'
-const MERCHANT_ID = 'M221LS4ADJ5UN'
-const SALT_KEY = 'ffc08980-85e0-4247-a999-be8f8fec8cc8'
+
 
 
 payController = async (req, res) => {
@@ -14,7 +16,7 @@ payController = async (req, res) => {
 
     console.log("merchant id", merchantTransactionId)
     const payLoad = {
-        "merchantId": MERCHANT_ID,
+        "merchantId": process.env.MERCHANT_ID,
         "merchantTransactionId": merchantTransactionId,
         "merchantUserId": userId,
         "amount": 100,
@@ -29,7 +31,7 @@ payController = async (req, res) => {
     const bufferObj = Buffer.from(JSON.stringify(payLoad), 'utf8');
     const base63EncodedPayLoad = bufferObj.toString('base64');
     console.log(base63EncodedPayLoad);
-    const xVerify = sha256(base63EncodedPayLoad + payEndPoint + SALT_KEY) + '###' + SALT_INDEX;
+    const xVerify = sha256(base63EncodedPayLoad + payEndPoint + process.env.SALT_KEY) + '###' + SALT_INDEX;
     console.log(xVerify);
 
     const options = {
@@ -65,41 +67,67 @@ payController = async (req, res) => {
 
 }
 
-statusController = (req, res) => {
-    const { merchantTransactionId } = req.params;
-    
-    if (merchantTransactionId) {
-        const xVerify = sha256(`/pg/v1/status/${MERCHANT_ID}/${merchantTransactionId}` + SALT_KEY) + '###' + SALT_INDEX;
-        const options = {
-            method: 'POST',
-            url: `${PHONE_PE_HOST_URL}/pg/v1/status/${MERCHANT_ID}/${merchantTransactionId}`,
-            headers: {
-                'Accept': 'application/json', // Specify that we expect JSON response
-                'Content-Type': 'application/json',
-                'X-MERCHANT-ID': MERCHANT_ID,
-                'X-VERIFY': xVerify,
-            },
-        };
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.ADMIN_EMAIL,
+        pass: process.env.ADMIN_EMAIL_PASSWORD, 
+    }
+});
 
-        axios
-            .request(options)
-            .then(function (response) {
-                console.log(response.data);
-                if (response.data.code === 'PAYMENT_SUCCESS') {
-                    // Handle payment success
-                    res.send('Payment successful');
-                } else if (response.data.code === 'PAYMENT_ERROR') {
-                    res.send('Payment error');
-                } else {
-                    res.send('Unknown status');
-                }
-            })
-            .catch(function (error) {
-                console.error('Error from PhonePe API:', error.response ? error.response.data : error.message);
-                res.status(500).send({ error: 'Internal server error' });
-            });
-    } else {
-        res.status(400).send({ error: 'Missing merchantTransactionId' });
+statusController = async (req, res) => {
+    try {
+        const { merchantTransactionId } = req.params;
+        if (merchantTransactionId) {
+            const xVerify = sha256(`/pg/v1/status/${process.env.MERCHANT_ID}/${merchantTransactionId}` + process.env.SALT_KEY) + '###' + SALT_INDEX;
+            const options = {
+                method: 'POST',
+                url: `${PHONE_PE_HOST_URL}/pg/v1/status/${process.env.MERCHANT_ID}/${merchantTransactionId}`,
+                headers: {
+                    'Content-Type': 'application/json',
+                    "X-MERCHANT-ID": process.env.MERCHANT_ID,
+                    'X-VERIFY': xVerify,
+                },
+            };
+
+            const response = await axios.request(options);
+            if (response.data.code === 'PAYMENT_SUCCESS') {
+                // Payment success, generate the order
+                const newOrder = new Order({
+                    userId: req.body.userId,
+                    orderId: merchantTransactionId,
+                    products: req.body.products, // Assuming you have product details in the request
+                    totalAmount: req.body.totalAmount, // Assuming total amount is in the request
+                    orderStatus: 'Processing',
+                    paymentStatus: 'Completed',
+                    address: req.body.address, // Assuming address details are in the request
+                    orderDate: new Date()
+                });
+
+                const savedOrder = await newOrder.save();
+
+                // Send email to admin with order details
+                const mailOptions = {
+                    from: process.env.ADMIN_EMAIL,
+                    to: process.env.ADMIN_EMAIL, // Admin email address
+                    subject: 'New Order Received',
+                    text: `A new order has been placed.\n\nOrder ID: ${savedOrder.orderId}\nTotal Amount: ${savedOrder.totalAmount}\nOrder Date: ${savedOrder.orderDate}\n\nPlease check the admin panel for more details.`,
+                };
+
+                await transporter.sendMail(mailOptions);
+
+                res.send('Payment Success and Order Created');
+            } else if (response.data.code === 'PAYMENT_ERROR') {
+                res.send('Payment Error');
+            } else {
+                res.status(500).send({ error: 'Unexpected status code' });
+            }
+        } else {
+            res.status(400).send({ error: 'Missing merchantTransactionId' });
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send({ error: 'Server Error' });
     }
 };
 
