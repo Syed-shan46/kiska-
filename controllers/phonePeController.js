4c7372c71d00303375dacd9328750eac9454936b
Syed-shan46 <159760172+Syed-shan46@users.noreply.github.com>
2024-08-17 10:58:37 +0530

    ad

--- controllers/phonePeController.js
+++ controllers/phonePeController.js
@@ -4,63 +4,69 @@ const sha256 = require('sha256');
 const PHONE_PE_HOST_URL = 'https://api.phonepe.com/apis/hermes'
 const SALT_INDEX = 1
 const payEndPoint = '/pg/v1/pay'
-const merchantTransactionId = uniqid();
+
 const MERCHANT_ID = 'M221LS4ADJ5UN'
 const SALT_KEY = 'ffc08980-85e0-4247-a999-be8f8fec8cc8'
 
 
 payController = async (req, res) => {
-    const userId = req.body.userId
 
+    try {
+        const userId = req.body.userId
 
-    console.log("merchant id", merchantTransactionId)
-    const payLoad = {
-        "merchantId": MERCHANT_ID,
-        "merchantTransactionId": merchantTransactionId,
-        "merchantUserId": userId,
-        "amount": 100,
-        "redirectUrl": `https://kiska.in/redirect-url/${merchantTransactionId}`,
-        "redirectMode": "POST",
-        "callbackUrl": `https://kiska.in/callback-url`,
-        "paymentInstrument": {
-            "type": "PAY_PAGE"
-        }
-    };
-
-    const bufferObj = Buffer.from(JSON.stringify(payLoad), 'utf8');
-    const base63EncodedPayLoad = bufferObj.toString('base64');
-    console.log(base63EncodedPayLoad);
-    const xVerify = sha256(base63EncodedPayLoad + payEndPoint + SALT_KEY) + '###' + SALT_INDEX;
-    console.log(xVerify);
-
-    const options = {
-        method: 'POST',
-        url: `${PHONE_PE_HOST_URL}${payEndPoint}`,
-        headers: {
-            'Content-Type': 'application/json',
-            'X-VERIFY': xVerify,
-        },
-        data: {
-            request: base63EncodedPayLoad,
-        }
-    };
-    axios
-        .request(options) 
-        .then(function (response) {
-            console.log(response.data);
-            if (response.data.data && response.data.data.instrumentResponse && response.data.data.instrumentResponse.redirectInfo) {
-                const url = response.data.data.instrumentResponse.redirectInfo.url;
-                res.redirect(url);
-            } else {
-                console.error('Unexpected response structure:', response.data);
-                res.send({ error: 'Unexpected response structure' });
+        console.log("merchant id", merchantTransactionId)
+        const merchantTransactionId = uniqid();
+        const payLoad = {
+            "merchantId": MERCHANT_ID,
+            "merchantTransactionId": merchantTransactionId,
+            "merchantUserId": userId,
+            "amount": 100,
+            "redirectUrl": `https://kiska.in/redirect-url/${merchantTransactionId}`,
+            "redirectMode": "POST",
+            "callbackUrl": `https://kiska.in/callback-url`,
+            "paymentInstrument": {
+                "type": "PAY_PAGE"
             }
-        })
-        .catch(function (error) {
-            console.error('Error from PhonePe API:', error.response ? error.response.data : error.message);
-            res.send(error);
-        });
+        };
 
+        const bufferObj = Buffer.from(JSON.stringify(payLoad), 'utf8');
+        const base63EncodedPayLoad = bufferObj.toString('base64');
+        console.log(base63EncodedPayLoad);
+        const xVerify = sha256(base63EncodedPayLoad + payEndPoint + SALT_KEY) + '###' + SALT_INDEX;
+        console.log(xVerify);
+
+        const options = {
+            method: 'POST',
+            url: `${PHONE_PE_HOST_URL}${payEndPoint}`,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'X-VERIFY': xVerify,
+            },
+            timeout: 5000,
+            data: {
+                request: base63EncodedPayLoad,
+            }
+        };
+        axios
+            .request(options)
+            .then(function (response) {
+                console.log(response.data);
+                if (response.data.data && response.data.data.instrumentResponse && response.data.data.instrumentResponse.redirectInfo) {
+                    const url = response.data.data.instrumentResponse.redirectInfo.url;
+                    res.redirect(url);
+                } else {
+                    console.error('Unexpected response structure:', response.data);
+                    res.send({ error: 'Unexpected response structure' });
+                }
+            })
+            .catch(function (error) {
+                console.error('Error from PhonePe API:', error.response ? error.response.data : error.message);
+                res.send(error);
+            });
+    } catch (error) {
+        res.send(error)
+    }
 
 }
 
