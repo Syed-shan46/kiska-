0f6fb8b5afcd1aacaf6d6ee5d053f375ba726bdb
Syed-shan46 <159760172+Syed-shan46@users.noreply.github.com>
2024-08-18 21:32:04 +0530

    safe

--- controllers/phonePeController.js
+++ controllers/phonePeController.js
@@ -5,9 +5,6 @@ const PHONE_PE_HOST_URL = 'https://api.phonepe.com/apis/hermes'
 const SALT_INDEX = 1;
 const payEndPoint = '/pg/v1/pay';
 
-const MERCHANT_ID = 'M221LS4ADJ5UN'
-
-const SALT_KEY = 'ffc08980-85e0-4247-a999-be8f8fec8cc8'
 
 
 
@@ -17,7 +14,7 @@ payController = async (req, res) => {
 
     console.log("merchant id", merchantTransactionId)
     const payLoad = {
-        "merchantId": MERCHANT_ID,
+        "merchantId": process.env.MERCHANT_ID,
         "merchantTransactionId": merchantTransactionId,
         "merchantUserId": userId,
         "amount": 100,
@@ -32,7 +29,7 @@ payController = async (req, res) => {
     const bufferObj = Buffer.from(JSON.stringify(payLoad), 'utf8');
     const base63EncodedPayLoad = bufferObj.toString('base64');
     console.log(base63EncodedPayLoad);
-    const xVerify = sha256(base63EncodedPayLoad + payEndPoint + SALT_KEY) + '###' + SALT_INDEX;
+    const xVerify = sha256(base63EncodedPayLoad + payEndPoint + process.env.SALT_KEY) + '###' + SALT_INDEX;
     console.log(xVerify);
 
     const options = {
@@ -68,43 +65,7 @@ payController = async (req, res) => {
 
 }
 
-statusController = (req, res) => {
-    const { merchantTransactionId } = req.params;
-    
-    if (merchantTransactionId) {
-        const xVerify = sha256(`/pg/v1/status/${MERCHANT_ID}/${merchantTransactionId}` + SALT_KEY) + '###' + SALT_INDEX;
-        const options = {
-            method: 'POST',
-            url: `${PHONE_PE_HOST_URL}/pg/v1/status/${MERCHANT_ID}/${merchantTransactionId}`,
-            headers: {
-                'Accept': 'application/json', // Specify that we expect JSON response
-                'Content-Type': 'application/json',
-                'X-MERCHANT-ID': MERCHANT_ID,
-                'X-VERIFY': xVerify,
-            },
-        };
 
-        axios
-            .request(options)
-            .then(function (response) {
-                console.log(response.data);
-                if (response.data.code === 'PAYMENT_SUCCESS') {
-                    // Handle payment success
-                    res.send('Payment successful');
-                } else if (response.data.code === 'PAYMENT_ERROR') {
-                    res.send('Payment error');
-                } else {
-                    res.send('Unknown status');
-                }
-            })
-            .catch(function (error) {
-                console.error('Error from PhonePe API:', error.response ? error.response.data : error.message);
-                res.status(500).send({ error: 'Internal server error' });
-            });
-    } else {
-        res.status(400).send({ error: 'Missing merchantTransactionId' });
-    }
-};
 
 callbackUrl = (req, res) => {
 
