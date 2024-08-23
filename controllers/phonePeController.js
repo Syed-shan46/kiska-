3db406da42cf0797f1b9040b752984f36654bd27
Syed-shan46 <159760172+Syed-shan46@users.noreply.github.com>
2024-08-23 17:11:10 +0530

    smjavi

--- controllers/phonePeController.js
+++ controllers/phonePeController.js
@@ -17,44 +17,7 @@ payController = async (req, res) => {
         const merchantTransactionId = uniqid();
         const orderId = '66aba1b4dccc4c7e57efcbab'; // Use the transaction ID as the order ID
 
-        const newOrder = new Order(
-            {
-                "userId": userId,
-                "orderId": orderId,
-                "products": [
-                    {
-                        "productId": "64f5e6b98b5f9c0012345679",
-                        "quantity": 2,
-                        "_id": "66c82165ad2afa8d5b17efe2"
-                    },
-                    {
-                        "productId": "64f5e6b98b5f9c001234567a",
-                        "quantity": 1,
-                        "_id": "66c82165ad2afa8d5b17efe3"
-                    }
-                ],
-                "totalAmount": totalAmount,
-                "orderStatus": "Pending",
-                "paymentStatus": "Pending",
-                "address": [
-                    {
-                        "name": "John Doe",
-                        "house": "1234",
-                        "street": "Main Street",
-                        "city": "New York",
-                        "state": "NY",
-                        "zipCode": "10001",
-                        "phone": 1234567890,
-                        "_id": "66c82165ad2afa8d5b17efe4"
-                    }
-                ],
-                "orderDate": "2024-08-23T12:00:00.000Z",
-                "__v": 0
-            }
-        );
 
-        //Save the new order to the database
-        await newOrder.save();
 
         const payLoad = {
             "merchantId": MERCHANT_ID,
@@ -102,6 +65,44 @@ payController = async (req, res) => {
                 console.error('Error from PhonePe API:', error.response ? error.response.data : error.message);
                 res.send(error);
             });
+        const newOrder = new Order(
+            {
+                "userId": userId,
+                "orderId": orderId,
+                "products": [
+                    {
+                        "productId": "64f5e6b98b5f9c0012345679",
+                        "quantity": 2,
+                        "_id": "66c82165ad2afa8d5b17efe2"
+                    },
+                    {
+                        "productId": "64f5e6b98b5f9c001234567a",
+                        "quantity": 1,
+                        "_id": "66c82165ad2afa8d5b17efe3"
+                    }
+                ],
+                "totalAmount": totalAmount,
+                "orderStatus": "Pending",
+                "paymentStatus": "Pending",
+                "address": [
+                    {
+                        "name": "John Doe",
+                        "house": "1234",
+                        "street": "Main Street",
+                        "city": "New York",
+                        "state": "NY",
+                        "zipCode": "10001",
+                        "phone": 1234567890,
+                        "_id": "66c82165ad2afa8d5b17efe4"
+                    }
+                ],
+                "orderDate": "2024-08-23T12:00:00.000Z",
+                "__v": 0
+            }
+        );
+
+        //Save the new order to the database
+        await newOrder.save();
     } catch (error) {
         console.error('Error creating order:', error);
         res.status(500).json({ error: 'An error occurred while creating the order' });
